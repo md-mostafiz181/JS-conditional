efd3dd514380d6c56c5795eeea2d88a5bd4e0fc1
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-24 10:48:21 +0600

    practice js conditional if else

---
+++ multiple.js
@@ -0,0 +1,15 @@
+const salary = 25000;
+const isBcs= true;
+const height= 66 ;
+
+if(salary > 20000 && height > 68){
+    console.log("valo chele so biye deya jay")
+}else{
+    console.log("beton kom so onno chele dekhte hobe")
+}
+
+if(salary > 22000 || height > 65 || isBcs == true){
+    console.log("asho baba biyedei ")
+}else{
+    console.log("kono vabei biye deya possible na ")
+}
