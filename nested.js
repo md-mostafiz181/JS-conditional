083641030785be746408ffd4d056f233276a9dcf
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-24 11:21:28 +0600

    practice nested if else condition

---
+++ nested.js
@@ -0,0 +1,15 @@
+const money = 0 ;
+
+if(money > 300){
+    console.log("tui beta borolok")
+}else{
+    if(money > 100 ){
+        console.log("tui beta majha majhi")
+    }else{
+        if(money > 20 ){
+            console.log( "tui beta fokira ")
+        }else{
+            console.log("tui amar bondhu na ")
+        }
+    }
+}
